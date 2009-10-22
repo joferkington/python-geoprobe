917a04255bae2dd5f18c8c5486eb48434fc0bb6f
Joe Kington <joferkington@gmail.com>
2009-10-21 21:36:44 -0500

    Fixed setup.py -- version should be a string

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'geoprobe',
-    version = 0.1,
+    version = '0.1',
     description = "Reads and (partially) writes seismic data in Landmark's Geoprobe format",
     author = 'Joe Kington',
     author_email = 'joferkington@gmail.com',
