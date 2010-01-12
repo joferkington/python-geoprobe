088047e18ff9e0c23f5220e4544b6c43065fc231
Joe Kington <joferkington@gmail.com>
2010-01-12 10:48:52 -0600

    Clarification in isValidVolume

--- geoprobe/volume.py
+++ geoprobe/volume.py
@@ -19,6 +19,7 @@ def isValidVolume(filename):
     volSize = os.stat(filename).st_size
     predSize = testvol.nx*testvol.ny*testvol.nz + _headerLength
 
+    # VolID == 43970 is a version 2 geoprobe volume (The only type currently supported)
     if (volID!=43970) or (volSize!=predSize): return False
     else: return True
 
