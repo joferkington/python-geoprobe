e4694f79877a11447f10be01d5871b361e32cba6
Joe Kington <joferkington@gmail.com>
2010-03-23 11:42:18 -0500

    Added license metadata to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description = "Reads and (partially) writes seismic data in Landmark's Geoprobe format",
     author = 'Joe Kington',
     author_email = 'joferkington@gmail.com',
+    license = 'MIT',
     url = 'http://code.google.com/p/python-geoprobe/',
     packages = ['geoprobe'],
     package_data = {'geoprobe' : ['__init__.py', 'volume.py', 'horizon.py', 
