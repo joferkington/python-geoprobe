2456136a3881a68c4df2ed92963955601145304b
Joe Kington <joferkington@gmail.com>
2010-04-22 17:34:18 -0500

    Made the docstring for geoprobe.data2d contain docs for all of the attributes created at initialization

--- geoprobe/data2d.py
+++ geoprobe/data2d.py
@@ -1,14 +1,20 @@
 import struct
 import numpy as np
 from common import BinaryFile
+from common import format_headerDef_docs
 from _2dHeader import headerDef as _headerDef
 from _2dHeader import headerLength as _headerLength
 
 class data2d(object):
-    """Reads geoprobe 2D data files.
-    data2d.data is a 2D numpy array containg the seismic data
-    data2d.x is a list of the x-coordinates of each trace
-    data2d.y is a list of the y-coordinates of each trace"""
+    # Not a "normal" docstring so that "useful attributes" is set at initialization
+    __doc__ = """
+    Reads geoprobe 2D data files.
+
+    Useful Attributes:
+        data: array containg the seismic data as uint8's
+        x: a list of the x-coordinates of each trace
+        y: a list of the y-coordinates of each trace\n%s""" % format_headerDef_docs(_headerDef)
+
     def __init__(self, filename):
         """
         Input:
@@ -19,6 +25,10 @@ class data2d(object):
         self._readTraces()
 
     def _readHeader(self):
+        """
+        Read the values stored in the file header and set each one as
+        an attribue of the data2d object.
+        """
         for varname, props in _headerDef.iteritems():
             offset, fmt = props['offset'], props['type']
             self._infile.seek(offset)
@@ -27,12 +37,20 @@ class data2d(object):
 
     @property
     def headerValues(self):
+        """
+        A dict of all the values stored in the file header
+        (Each of these is also an attribute of any data2d object)
+        """
         output = {}
         for varname in _headerDef.keys():
             output[varname] = getattr(self, varname)
         return output
         
     def _readTraces(self):
+        """
+        Read all traces (everything other than the file header) 
+        from self._infile
+        """
         dtype = [('x', '>f4'), ('y', '>f4'), ('tracenum', '>f4'), ('traces', '%i>u1'%self._numSamples)]
         self._infile.seek(_headerLength)
         data = np.fromfile(self._infile, dtype=dtype, count=self._numTraces)
@@ -42,8 +60,16 @@ class data2d(object):
 
     @property
     def numTraces(self):
+        """
+        The number of traces stored in the file.
+        Equivalent to self.data.shape[0]
+        """
         return self.data.shape[0]
 
     @property
     def numSamples(self):
+        """
+        The number of samples in each trace stored in the file
+        Equivalent to self.data.shape[1]
+        """
         return self.data.shape[1]
